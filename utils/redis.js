59a90ba5e45b70ad3ebd68a5fd4e8f16ef0018aa
Eman288 <emantamam107@gmail.com>
2024-12-11 12:02:33 -0800

    try task 0

---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+import {createClient} from 'redis';
+
+class RedisClient {
+	constructor() {
+		this.client = createClient();
+	}
+
+	this.client.connect().catch((err) => {
+		console.error(`Failed to Connect to Redis: ${err.message}`);
+	});
+
+	isAlive() {
+		return this.client.isReady;
+	}
+
+	async get(key) {
+		return await this.client.get(key);
+	}
+
+	async set(key, value, time) {
+		await this.client.set(key, value, { EX: time }));
+	}
+
+	async del(key) {
+		await this.client.del(key);
+	}
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
