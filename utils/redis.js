4e7667c094739276e66d777d3a476714dbb18fd4
Eman288 <emantamam107@gmail.com>
2024-12-11 12:38:38 -0800

    trying to fix the promicises

--- utils/redis.js
+++ utils/redis.js
@@ -4,32 +4,53 @@ class RedisClient {
 	constructor() {
 		this.client = createClient();
 
-		this.client.on('error', (err) => {
-            console.error(`Redis client error: ${err.message}`);
+		this.client.on('error', (error) => {
+            console.error(`Redis client error: ${error.message}`);
         });
-
-
-		this.client.connect().catch((err) => {
-			console.error(`Failed to Connect to Redis: ${err.message}`);
-		});
 	}
 
 	isAlive() {
-		return this.client.isReady;
+		return this.client.connected;
 	}
 
 	async get(key) {
-		return await this.client.get(key);
+		return new Promise((resolve, reject) => {
+	      	this.client.get(key, (error, reply) => {
+        	if (error) {
+          		reject(error);
+        	} else {
+          		resolve(reply);
+        	}
+      		});
+    		});
 	}
 
-	async set(key, value, time) {
-		await this.client.set(key, value, { EX: time });
-	}
+	async set(key, value, durationInSeconds) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, durationInSeconds, value, (error, reply) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+	
 
 	async del(key) {
-		await this.client.del(key);
+		return new Promise((resolve, reject) => {
+			this.client.del(key, (error, reply) => {
+				if (error) {
+                                        reject(error);
+				} else {
+                                        resolve(reply);
+                                }
+                                });
+                                });
+			}
 	}
-}
+
 
 const redisClient = new RedisClient();
 export default redisClient;
